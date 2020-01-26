69d7d115d3c4d511e421094fcdd969cddfa9350f
caitlin-hutnyk <caitlin.hutnyk@gmail.com>
2020-01-26 14:31:25 -0500

    added gitignore and some work

--- LogRegression.py
+++ LogRegression.py
@@ -5,8 +5,15 @@ class Log_Regression:
 	def __init__(self):
 		pass
 
+	# training
+	# X: NxD training data
+	# y: Nx1 discrete targets (for now y in {0,1})
 	def fit(X, y):
 		pass
 
-	def predict(X, y):
-		pass
+	# predict and return y_hat
+	# X: NxD test data
+	def predict(X):
+		n,d = np.shape(X)[0], np.shape(X)[1]
+		y_hat = np.zeros((n,1))
+		
