80f25d238fd681f3a6adac9f8acaa6bee77fa0cd
Hanna Navissi <h.navissi@student.unimelb.edu.au>
2020-02-06 14:18:24 -0500

    import fixed and desquiggled

--- Importv2.py
+++ Importv2.py
@@ -7,111 +7,81 @@ from sklearn.compose import ColumnTransformer
 # dataset 1: ionosphere data, uncomment to use
 # filename = 'data/ionosphere.data'
 # columns = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
- #          21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 'assessment']
+#          21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 'assessment']
 # columns = list(map(str, columns))
 
 # dataset 2: census data, uncomment to use
 filename = 'data/adult.data'
 columns = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation',
-            'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country',
-            'salary']
+           'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country',
+           'salary']
 
-# read in data and replace unknown values with NaN to be easily removed
-data = pd.read_csv(filename, sep=",", na_values=[" ?", "?"], names=columns)
-old_instances = data.shape[0]
-print(old_instances)
-
-# remove all instances with missing data
-data = data.dropna(axis=0, how="any")
-instances = data.shape[0]
-
-print("Rows dropped: %d" % (old_instances - instances))
-print(data.shape)
-print("Original row:")
-# to test any row, change the var
-print(data.iloc[0])
-
-# create separate y label array
-y_label = data.iloc[:, -1]
-y_label = y_label.values
-y_label = y_label.reshape((y_label.shape[0], 1))
-print(y_label.shape)
-
-# remove y label from data
-data = data.iloc[:, :-1]
-print("data shape with y removed")
-print(data.shape)
-
-categorical_feature_mask = []
-# convert categorical values to binary
-categorical_feature_mask = (data.dtypes == object)
-print(categorical_feature_mask)
-
-categorical_cols = data.columns[categorical_feature_mask].tolist()
-continuous_cols = data.columns[~categorical_feature_mask].tolist()
-
-print(categorical_cols)
-
-categorical_cols_index = []
-continuous_cols_index = []
-index = 0
-
-for col in categorical_feature_mask:
-    if col is True:
-        categorical_cols_index.append(index)
-    else:
-        continuous_cols_index.append(index)
-    index = index + 1
-
-data_continuous = np.ones((instances, 1))
-for i in continuous_cols_index:
-    print("i is {} ".format(i))
-    new_col = data.values[:, i]
-    col_reshape = new_col.reshape((data.shape[0], 1))
-    data_continuous = np.append(data_continuous, col_reshape, axis=1)
-
-data_continuous = data_continuous[:, 1:]
-print(data_continuous.shape)
-
-# here we have all our continuous vars in one array, and we drop them in the column transformer
-print("continuous data array shape:")
-print(data_continuous.shape)
-ct = ColumnTransformer([('a', OneHotEncoder(sparse=False), categorical_cols)], remainder='passthrough')
-data_categorical = ct.fit_transform(data)
-
-print(data_categorical)
+filename = 'poker'
 
 
-np.set_printoptions(precision=5)
-print("original data row 0:")
-print(data.iloc[0])
-print("continuous columns")
-print(data_continuous[0])
+def read_data():
+    # read in data and replace unknown values with NaN to be easily removed
+    data = pd.read_csv(filename, sep=",", na_values=[" ?", "?"], names=columns)
+    old_instances = data.shape[0]
 
+    # remove all instances with missing data
+    data = data.dropna(axis=0, how="any")
+    instances = data.shape[0]
 
-# split into training and testing set by 80:20 split
-mask = np.random.rand(data_categorical.shape[0]) < 0.8
-train_validate_categorical = data_categorical[mask]
-train_validate_continuous = data_continuous[mask]
+    print("Rows dropped: %d" % (old_instances - instances))
+    print(data.shape)
 
-# final
-test_categorical = data_categorical[~mask]
-test_continuous = data_continuous[~mask]
+    # create separate y label array
+    y_label = data.iloc[:, -1]
+    y_label = y_label.values
+    y_label = y_label.reshape((y_label.shape[0], 1))
 
-# ensure split is accurate
-assert (train_validate_categorical.shape[0] + test_categorical.shape[0] == instances)
+    # remove y label from data
+    data = data.iloc[:, :-1]
 
-print("training set: {0} testing set: {1} ".format(train_validate_categorical.shape[0], test_categorical.shape[0]))
+    # identify categorical and continuous features
+    categorical_feature_mask = []
+    categorical_feature_mask = (data.dtypes == object)
+    categorical_cols = data.columns[categorical_feature_mask].tolist()
+    continuous_cols = data.columns[~categorical_feature_mask].tolist()
 
-# split into training and validation set by 80:20 split
-mask = np.random.rand(train_validate_categorical.shape[0]) < 0.8
+    # identify index of each columns
+    categorical_cols_index = []
+    continuous_cols_index = []
+    index = 0
+    for col in categorical_feature_mask:
+        if col is True:
+            categorical_cols_index.append(index)
+        else:
+            continuous_cols_index.append(index)
+        index = index + 1
 
-# final
-train_categorical = train_validate_categorical[mask]
-validation_categorical = train_validate_categorical[~mask]
-train_continuous = train_validate_continuous[mask]
-validation_continuous = train_validate_continuous[~mask]
+    # create one col for data_continuous so that we can append further cols
+    data_continuous = np.ones((instances, 1))
+    for i in continuous_cols_index:
+        new_col = data.values[:, i]
+        col_reshape = new_col.reshape((data.shape[0], 1))
+        data_continuous = np.append(data_continuous, col_reshape, axis=1)
 
-# ensure split is accurate
-assert (train_categorical.shape[0] + validation_categorical.shape[0] == train_validate_categorical.shape[0])
-print("training set: {0} validation set: {1} ".format(train_categorical.shape[0], validation_categorical.shape[0]))
+    # drop the first col
+    data_continuous = data_continuous[:, 1:]
+
+    # here we have all our continuous vars in one array, so we can drop them
+    ct = ColumnTransformer([('dummy_col', OneHotEncoder(sparse=False), categorical_cols)], remainder='passthrough')
+    data_categorical = ct.fit_transform(data)
+
+    # we now have our data separated as continuous and categorical
+
+    # split into training and testing set by 80:20 split
+    mask = np.random.rand(data_categorical.shape[0]) < 0.8
+    train_validate_categorical = data_categorical[mask]
+    train_validate_continuous = data_continuous[mask]
+
+    # final test data
+    test_categorical = data_categorical[~mask]
+    test_continuous = data_continuous[~mask]
+
+    # ensure split is accurate
+    assert (train_validate_categorical.shape[0] + test_categorical.shape[0] == instances)
+
+    return y_label, test_categorical, test_continuous, train_validate_categorical, train_validate_continuous
--- LogRegression.py
+++ LogRegression.py
@@ -6,7 +6,6 @@ class Log_Regression:
 		self.learning_rate = lr
 		self.stop_cond = eps
 		self.w = -1
-		return self
 
 	# w Dx1
 	# x DxN
@@ -14,18 +13,18 @@ class Log_Regression:
 	def logistic(w, x):
 		return 1/(1 + np.exp(-np.dot(np.transpose(w), x)))
 
-	def gradient(X, y, w):
+	def gradient(self, X, y, w):
 		N,D = X.shape
-		yh = logistic(w,X)
+		yh = self.logistic(w, X)
 		grad = np.dot(np.transpose(X), yh - y)
 		return grad
 
-	def gradient_descent(X, y, lr, eps):
+	def gradient_descent(self, X, y, lr, eps):
 		N,D = X.shape
 		w = np.zeros(D)
 		g = np.inf 
 		while np.linalg.norm(g) > eps:
-			g = gradient(X,y,w)
+			g = self.gradient(X,y,w)
 			w = w - lr*g
 		return w
 
@@ -46,19 +45,18 @@ class Log_Regression:
 	# training
 	# X: NxD training data
 	# y: Nx1 discrete targets (for now y in {0,1})
-	def fit(X, y):
-		self.w = gradient_descent(X,y,self.learning_rate, self.stop_cond)
+	def fit(self, X, y):
+		self.w = self.gradient_descent(X,y,self.learning_rate, self.stop_cond)
 
 	# predict and return y_hat
 	# X: NxD test data
-	def predict(X):
+	def predict(self, X):
 		n,d = np.shape(X)
 		y_hat = np.zeros((n,1))
-		log = logistic(self.w,X)
+		log = self.logistic(self.w,X)
 		for i in range(n):
 			if log[i] >= 0.5:
 				y_hat[i] = 1
 			else:
 				y_hat[i] = 0
 		return y_hat
-		
