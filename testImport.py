fix adding the intercept. and make logreg track its average iterations for gradient descent

testImport.py
@@ -43,8 +43,8 @@ def read_data(which, type_):
 		test_x_cat = None
 
 		if type_:
-			train_x = np.append(np.ones((train_x_con[0], 1)), train_x_con, axis=1)
-			text_x = np.append(np.ones((test_x_con[0], 1)), test_x_con, axis=1)
+			train_x = np.append(np.ones((train_x_con.shape[0], 1)), train_x_con, axis=1)
+			test_x = np.append(np.ones((test_x_con.shape[0], 1)), test_x_con, axis=1)
 			return train_x, train_y, test_x, test_y
 		return train_x_con, train_x_cat, train_y, test_x_con, test_x_cat, test_y
 
@@ -84,9 +84,9 @@ def read_data(which, type_):
 
 		if type_:
 			train_x = np.append(train_x_con, train_x_cat, axis=1)
-			train_x = np.append(np.ones((train_x[0], 1)), train_x, axis=1)
+			train_x = np.append(np.ones((test_x.shape[0], 1)), test_x, axis=1)
 			test_x = np.append(test_x_con, test_x_cat, axis=1)
-			test_x = np.append(np.ones((train_x[0], 1)), train_x, axis=1)
+			test_x = np.append(np.ones((test_x.shape[0], 1)), test_x, axis=1)
 			return train_x, train_y, test_x, test_y
 		return train_x_con, train_x_cat, train_y, test_x_con, test_x_cat, test_y
 
@@ -123,8 +123,8 @@ def read_data(which, type_):
 		test_x_cat = np.array(data_new_arr[split:,:-10])
 
 		if type_:
-			train_x = np.append(np.ones((train_x_cat[0], 1)), train_x_cat, axis=1)
-			text_x = np.append(np.ones((test_x_cat[0], 1)), test_x_cat, axis=1)
+			train_x = np.append(np.ones((train_x_cat.shape[0], 1)), train_x_cat, axis=1)
+			test_x = np.append(np.ones((test_x_cat.shape[0], 1)), test_x_cat, axis=1)
 			return train_x, train_y, test_x, test_y
 		return train_x_con, train_x_cat, train_y, test_x_con, test_x_cat, test_y
 
@@ -162,8 +162,8 @@ def read_data(which, type_):
 
 		if type_:
 			train_x = np.append(train_x_con, train_x_cat, axis=1)
-			train_x = np.append(np.ones((train_x[0], 1)), train_x, axis=1)
+			train_x = np.append(np.ones((train_x.shape[0], 1)), train_x, axis=1)
 			test_x = np.append(test_x_con, test_x_cat, axis=1)
-			test_x = np.append(np.ones((train_x[0], 1)), train_x, axis=1)
+			test_x = np.append(np.ones((test_x.shape[0], 1)), test_x, axis=1)
 			return train_x, train_y, test_x, test_y
 		return train_x_con, train_x_cat, train_y, test_x_con, test_x_cat, test_y